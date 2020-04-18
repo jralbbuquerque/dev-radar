3942f5ddd4d5bc20c7d1a665facb04a102586037
Francisco Júnior <jralbbuquerque@gmail.com>
2020-04-18 15:32:15 -0300

    Create the backend of aplication

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://jralbbuquerque:jr88496944@cluster0-aiydm.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+/** Tipos de parâmetros 
+ * Query params: request.query (Filtros, ordenação, paginação...)
+ * Rotue params: request.params (Indentificar um recurso na alteração ou remoção)
+ * Body: request.body (Dados para criação ou alteração de um registro)
+*/ 
+
+// MongoDB (bando de dados não relacional)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
---
+++ backend/src/models/utils/PointSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const PoitSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+    },
+    coordinates: {
+        // Longitude, Latitude
+        type: [Number], 
+        required: true,
+    },
+});
+
+module.exports = PoitSchema;
