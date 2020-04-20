a2e32bcadd6d50674479b43122293c4520159ccc
Francisco Júnior <jralbbuquerque@gmail.com>
2020-04-20 11:20:52 -0300

    Front-end initialization and sidebar creation

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css'
+
+/** Conceitos do React
+ * Componentes: são funções que retornam HTML
+ * Propriedade: Informações que um componente PAI passa para o componente filho
+ * Estado: Informações mantidas pelo componente (Lembrar: Imutabilidade)
+ */
+
+function App() {
+    return (
+        <div id="app">
+            <aside>
+                <strong>Cadastrar</strong>
+                <form>
+                    <div class="input-block">
+                        <label htmlFor="github_username">Usuário do Github</label>
+                        <input name="github_username" id="github_username" required />
+                    </div>
+
+                    <div class="input-block">
+                        <label htmlFor="techs">Tecnologias</label>
+                        <input name="techs" id="techs" required />
+                    </div>
+
+                    <div className="input-group">
+                        <div class="input-block">
+                            <label htmlFor="latitude">Latitude</label>
+                            <input name="latitude" id="latitude" required />   
+                        </div>
+
+                        <div class="input-block">
+                            <label htmlFor="longitude">Longitude</label>
+                            <input name="longitude" id="longitude" required />   
+                        </div>
+                    </div>
+
+                    <button type="submit">Salvar</button>
+                </form>
+            </aside>
+            <main>
+                
+            </main>
+        </div>
+    );
+}
+
+export default App;
